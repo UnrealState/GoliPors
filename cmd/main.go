07ac347c1be8b47fd9c1e5e6cd8f187927a0a8a2
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-12-01 03:20:55 +0330

    feat: Init config in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"golipors/config"
+	"os"
+)
 
+var configPath = flag.String("config", "config.json", "Path to service config file")
+
+func main() {
+	flag.Parse()
+
+	if v := os.Getenv("CONFIG_PATH"); len(v) > 0 {
+		*configPath = v
+	}
+
+	_ = config.MustReadConfig(*configPath)
 }
