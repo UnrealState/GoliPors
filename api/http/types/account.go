cbf75d70d9c92471fc18789855035200508af883
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-12-06 18:47:21 +0330

    feat(api): Implement verify password types, endpoint and service

--- api/http/types/account.go
+++ api/http/types/account.go
@@ -11,3 +11,14 @@ type LoginResponse struct {
 	Code      string    `json:"code"`
 	SessionId uuid.UUID `json:"session_id"`
 }
+
+type VerifyOTPRequest struct {
+	Email     string    `json:"email" validate:"required,email"`
+	Code      string    `json:"code" validate:"required"`
+	SessionId uuid.UUID `json:"session_id"`
+}
+
+type VerifyOTPResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
